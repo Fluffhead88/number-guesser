f496cecfd7e3874b8968356454ed2798839f4e11
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-15 15:10:34 -0400

    Initial Hard Version of Number Guess Game

---
+++ number_guesser/number_guesser_hard.py
@@ -0,0 +1,22 @@
+my_number = 30
+
+import random
+
+computer_number = random.randint(0, 100)
+counter = 0
+
+
+for guess in range(5):
+    guess = guess + 1
+    if computer_number < my_number:
+        print ("That's too low.")
+    if computer_number > my_number:
+        print ("That's too high.")
+    if computer_number == my_number:
+        print ("That's correct!")
+        break
+    computer_number = random.randint(0, 100)
+    counter = counter + 1
+
+
+print(counter)
