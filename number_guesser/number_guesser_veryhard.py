07bffe14188772d9afbe9e22bfe2d75ac596dbda
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-15 16:09:20 -0400

    Getting closer to figuring out the solution. Adjusting the range of guesses

--- number_guesser/number_guesser_veryhard.py
+++ number_guesser/number_guesser_veryhard.py
@@ -7,15 +7,13 @@ counter = 0
 
 
 for guess in range(5):
-    computer_number = random.randint(0, 100)
-    guess = computer_number
     counter = counter + 1
     if computer_number < my_number:
         print ("That's too low.")
-        computer_number = random.randint(guess, 100)
+        computer_number_2 = random.randint(computer_number, 100)
     if computer_number > my_number:
         print ("That's too high.")
-        computer_number = random.randint(0, guess)
+        computer_number_3 = random.randint(computer_number_2, computer_number)
     if computer_number == my_number:
         print ("That's correct!")
         break
